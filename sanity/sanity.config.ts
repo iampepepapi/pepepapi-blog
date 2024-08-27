34f9f896eafc6975491e3673e678e731f10cfb35
Samuel Ismael Gonzalez Ampuero <samuel.iga03@gmail.com>
2024-08-27 07:34:18 +0200

    lets try again

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { visionTool } from '@sanity/vision'
+import { visionTool } from '@sanity/vision/'
 import { schemaTypes } from './schemaTypes'
 import { markdownSchema } from "sanity-plugin-markdown";
 import { codeInput } from '@sanity/code-input'
